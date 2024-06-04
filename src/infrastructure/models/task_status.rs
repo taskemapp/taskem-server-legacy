21f8e1e44ccedd33cd19eb4b7cae27976f78986c
ripls56 <84716344+ripls56@users.noreply.github.com>
2024-06-04 16:10:38 +0300

    fix: infer type for type parameter `DB`

--- src/infrastructure/models/task_status.rs
+++ src/infrastructure/models/task_status.rs
@@ -3,6 +3,7 @@ use crate::infrastructure::schema::sql_types::TaskStatus as TaskStatusScheme;
 use diesel::deserialize::FromSql;
 use diesel::pg::{Pg, PgValue};
 use diesel::serialize::{IsNull, Output, ToSql};
+use diesel::sql_types::Text;
 use diesel::{AsExpression, FromSqlRow};
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter};
@@ -69,7 +70,7 @@ impl ToSql<TaskStatusScheme, Pg> for TaskStatusDiesel {
 
 impl FromSql<TaskStatusScheme, Pg> for TaskStatusDiesel {
     fn from_sql(bytes: PgValue) -> diesel::deserialize::Result<Self> {
-        let binding: String = String::from_sql(bytes)?;
+        let binding = <String as FromSql<Text, Pg>>::from_sql(bytes)?;
         let status = binding.as_str();
         match status {
             "in progress" => Ok(TaskStatusDiesel::InProgress),
